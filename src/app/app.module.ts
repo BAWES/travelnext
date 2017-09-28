2f2fdabe7b26d74e263d926bb13a0242ba9dfea1
Khalid <khalid@bawes.net>
2017-09-28 11:52:29 +0300

    Setup firebase config

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,6 +11,16 @@ import { TabsPage } from '../pages/tabs/tabs';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
+// AF2 Dev Env Settings 
+export const firebaseConfig = {
+  apiKey: "AIzaSyBPnYYxbfSSDSQ1ALWcPqu54DFTUOH7Iy8",
+  authDomain: "traveldiscuss-dc79d.firebaseapp.com",
+  databaseURL: "https://traveldiscuss-dc79d.firebaseio.com",
+  projectId: "traveldiscuss-dc79d",
+  storageBucket: "traveldiscuss-dc79d.appspot.com",
+  messagingSenderId: "875187117160"
+};
+
 @NgModule({
   declarations: [
     MyApp,
