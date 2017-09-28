f74a0d8d2748cedc0266fc0a7fbb184364e79902
Khalid <khalid@bawes.net>
2017-09-28 13:38:38 +0300

    Display country list

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,18 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
+import { AngularFireDatabase, FirebaseListObservable } from 'angularfire2/database';
+
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  public countries: FirebaseListObservable<any[]>;
 
+  constructor(public navCtrl: NavController, public db: AngularFireDatabase) {
+    this.countries = this.db.list("/countries");
   }
 
 }
