28213f962fb594899257a432361e05458ed5c125
Khalid <khalid@bawes.net>
2017-09-29 18:07:02 +0300

    Add search between countries

--- src/pages/world/world.ts
+++ src/pages/world/world.ts
@@ -11,9 +11,40 @@ import { CountryService } from '../../providers/country.service';
 })
 export class WorldPage {
 
+  public countriesByRegion;
+
   constructor(
     public navCtrl: NavController, 
-    public countrySrvc: CountryService) {
+    public countrySrvc: CountryService
+  ) {
+      this.countriesByRegion = this.countrySrvc.countriesByRegion;
+  }
+
+  /**
+   * Search for vendor that matches user input
+   * @param  
+   */
+  search($event){
+    let userInput = $event.target.value;
+    if(!userInput){
+      this.countriesByRegion = this.countrySrvc.countriesByRegion;
+      return;
+    }
+
+    this.countriesByRegion = [];
+    this.countrySrvc.countriesByRegion.forEach(region => {
+      let filteredCountries = region.countries.filter(country => {
+        if((country.name.common.toLowerCase().indexOf(userInput.toLowerCase()) !== -1)){
+          return true;
+        }
+        return false;
+      });
+      // If there are results from filtered countries, push them to the list
+      if(filteredCountries.length){
+        region.countries = filteredCountries;
+        this.countriesByRegion.push(region);
+      }
+    });
   }
 
 }
