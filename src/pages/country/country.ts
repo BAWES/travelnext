f172f28edb59b68d1110dac976de0f5a08a429b0
Khalid <khalid@bawes.net>
2017-10-01 20:14:17 +0300

    Initial country page

--- src/pages/country/country.ts
+++ src/pages/country/country.ts
@@ -1,9 +1,7 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, NavParams } from 'ionic-angular';
 
 import { CountrySelectionPage } from '../country-selection/country-selection';
-
-import { CountryService } from '../../providers/country.service';
 
 @Component({
   selector: 'page-country',
@@ -11,43 +9,14 @@ import { CountryService } from '../../providers/country.service';
 })
 export class CountryPage {
 
-  public countriesByRegion;
+  public country;
 
   constructor(
-    public navCtrl: NavController, 
-    public countrySrvc: CountryService
+    public navCtrl: NavController,
+    params: NavParams 
   ) {
-      this.countriesByRegion = this.countrySrvc.countriesByRegion.slice(0);
-  }
-
-  /**
-   * Search for country that matches user input
-   * @param  
-   */
-  search($event){
-    let userInput = $event.target.value;
-
-    if(!userInput){
-      this.countriesByRegion = this.countrySrvc.countriesByRegion;
-      return;
-    }
-
-    this.countriesByRegion = [];
-    this.countrySrvc.countriesByRegion.forEach(region => {
-      let filteredCountries = region.countries.filter(country => {
-        if((country.name.common.toLowerCase().indexOf(userInput.toLowerCase()) !== -1)){
-          return true;
-        }
-        return false;
-      });
-      // If there are results from filtered countries, push them to the list
-      if(filteredCountries.length){
-        let newCountryData = JSON.parse(JSON.stringify(filteredCountries));
-        let newRegionData = JSON.parse(JSON.stringify(region));
-        newRegionData.countries = filteredCountries;
-        this.countriesByRegion.push(newRegionData);
-      }
-    });
+      this.country = params.get("country");
+      console.log(this.country);
   }
 
 }
