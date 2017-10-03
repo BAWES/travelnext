41bb9458076deb2b80ba303295c6c6298b939f6d
Khalid <khalid@bawes.net>
2017-10-03 20:46:30 +0300

    Friends page on null

--- src/pages/friends/friends.ts
+++ src/pages/friends/friends.ts
@@ -1,22 +1,52 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
-import { AngularFireDatabase, FirebaseListObservable } from 'angularfire2/database';
+import { AngularFireDatabase } from 'angularfire2/database';
 
 import { UserProfilePage } from '../user-profile/user-profile';
 import { FriendSearchPage } from '../friend-search/friend-search';
+
+import { AuthService } from '../../providers/auth.service'
 
 @Component({
   selector: 'page-friends',
   templateUrl: 'friends.html'
 })
 export class FriendsPage {
+  public friendDataLoaded = false;
 
-  public friendList: FirebaseListObservable<any>;
-
+  public user;
+  public friendData = [];
+  
+  public userSubscription;
+  public followingSubscription;
+  
   constructor(
     public navCtrl: NavController, 
-    public db: AngularFireDatabase
+    public db: AngularFireDatabase,
+    private _auth: AuthService
   ) {
+    this.setupSubscriptions();
+  }
+
+  ionViewWillUnload(){
+    this.userSubscription.unsubscribe();
+    this.followingSubscription.unsubscribe();
+  }
+
+  setupSubscriptions(){
+    this.userSubscription = this.db.object(`/users/${this._auth.uid}`).subscribe((result) => {
+      this.user = result;
+    });
+
+    this.followingSubscription = this.db.list(`/user-following/${this._auth.uid}`).subscribe((friendlist) => {
+      this.friendDataLoaded = true;
+      this.friendData = [];
+      friendlist.forEach(friend => {
+        this.db.object(`/users/${friend.$key}`).take(1).subscribe(userData => {
+          this.friendData.push(userData);
+        });
+      });
+    });
   }
 
   loadUserPage(user){
